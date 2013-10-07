b6e0ba87d93b1ac87233616c96714552ca88173a
heiyi <goheiyi@gmail.com>
2013-10-08 01:26:39 +0800

    2013-10-8

--- init-css.el
+++
@@ -1,7 +0,0 @@
-;; rainbow-mode needs color.el, bundled with Emacs >= 24.
-(require-package 'rainbow-mode)
-
-(dolist (hook '(css-mode-hook html-mode-hook))
-   (add-hook hook 'rainbow-mode))
-
-(provide 'init-css)
--- init-spelling.el
+++
@@ -1,8 +0,0 @@
-(require 'ispell)
-
-(when (executable-find ispell-program-name)
-  (require 'init-flyspell))
-
-(ispell-change-dictionary "american" t)
-
-(provide 'init-spelling)
