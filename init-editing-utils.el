c9327be373a5617481525573125382cd2d266684
heiyi <goheiyi@gmail.com>
2013-11-03 19:38:52 +0800

    2013-11-3

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -7,21 +7,18 @@
  column-number-mode t
  indent-tabs-mode nil
  size-indication-mode t
- show-trailing-whitespace t     ;;delete-trailing-space
+ show-trailing-whitespace t    ;; delete-trailing-space
  ediff-window-setup-function 'ediff-setup-windows-plain
  visible-bell t)
 
-(electric-pair-mode t)
-
-(setq ispell-dictionary "american")
+(blink-cursor-mode 0)
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
+(setq ispell-dictionary "american")
+
 ;; Auto reload files
 (global-auto-revert-mode 1)
-
-;; No cursor Blinking
-(blink-cursor-mode 0)
 
 ;; Enable ShowParenMode
 (setq show-paren-delay 0)
@@ -35,22 +32,14 @@
 ;;----------------------------------------------------------------------------
 ;; Emacs is Magic
 ;;----------------------------------------------------------------------------
-;; Expand region
+(cua-selection-mode t)
+
 (require-package 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
 
-;; undo-tree
 (require-package 'undo-tree)
 (global-undo-tree-mode)
 
-;; C-x C-u, C-x C-l
-(put 'upcase-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
-
-;; C-Enter
-(cua-selection-mode t)
-
-;; Go to char--->C-;
 (require-package 'ace-jump-mode)
 (global-set-key (kbd "C-;") 'ace-jump-mode)
 
@@ -60,28 +49,18 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
+;; C-x C-u, C-x C-l
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
+;; C-x n n --> C-x n w, inset ^L --> C-q C-l
+(put 'narrow-to-region 'disabled nil)
+
 ;; Handy key bindings
 (global-set-key (kbd "C-c j") 'join-line)
 (global-set-key (kbd "C-.") 'set-mark-command)
-
-;; C-x C-b ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
-;; C-o
 (define-key isearch-mode-map (kbd "C-o") 'isearch-occur)
-
-;; Move page --> C-x[, C-x], inset ^p --> C-q C-l, C-x n w exit narrow
-(put 'narrow-to-region 'disabled nil)    ;;C-x n n
-;;-------------------------- C-M-u------------------------------------
-(defun backward-up-sexp (arg)
-  "Jump up to the start of the ARG'th enclosing sexp."
-  (interactive "p")
-  (let ((ppss (syntax-ppss)))
-    (cond ((elt ppss 3)
-           (goto-char (elt ppss 8))
-           (backward-up-sexp (1- arg)))
-          ((backward-up-list arg)))))
-
-(global-set-key [remap backward-up-list] 'backward-up-sexp)
 
 (provide 'init-editing-utils)
--- init-program.el
+++ init-program.el
@@ -1,6 +1,13 @@
+;; For Lisp
+(require-package 'paredit)
+
+(add-hook 'scheme-mode-hook      #'enable-paredit-mode)
+(add-hook 'emacs-lisp-mode-hook  #'enable-paredit-mode)
+
 ;; For Css
 (require-package 'rainbow-mode)
+
 (dolist (hook '(css-mode-hook html-mode-hook))
-   (add-hook hook 'rainbow-mode))
+  (add-hook hook 'rainbow-mode))
 
 (provide 'init-program)
