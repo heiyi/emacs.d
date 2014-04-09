3ffff6b68b155c6ff4f5be796bdb34ccb20e3205
heiyi <goheiyi@gmail.com>
2014-04-10 01:41:48 +0800

    2014-4-10

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; reference purcell's emacs configuration
-(add-to-list 'load-path (expand-file-name "~/.emacs.d"))
+(add-to-list 'load-path "~/.emacs.d/")
 
 ;; Machinery for installing required
 (require 'init-elpa)
@@ -7,7 +7,5 @@
 ;; Load configs for specific features and modes
 (require 'init-gui)               ;; UI, Winner-Mode, Switch-Window
 (require 'init-ido)               ;; Smex && Hippie Expand
-(require 'init-editing-utils)	  ;; C->, C-c C-<, M-m, C-c j, C-;
-
-(require 'init-program)
-;;---------------------------------END------------------------------------
+(require 'init-editing-utils)	    ;; C->, C-c C-<, M-m, C-c j, C-;
+;;------------------------------END--------------------------------
