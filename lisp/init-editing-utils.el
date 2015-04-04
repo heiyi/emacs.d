f96ab438454a9653629814c349a10b2a939aa39c
heiyi <goheiyi@gmail.com>
2015-04-04 19:16:09 +0800

    2015-4-4

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -6,14 +6,11 @@
  auto-save-default nil
  column-number-mode t
  indent-tabs-mode nil
- size-indication-mode t
  show-trailing-whitespace t    ;; delete-trailing-space
  ediff-window-setup-function 'ediff-setup-windows-plain
  visible-bell t)
 
 (fset 'yes-or-no-p 'y-or-n-p)
-
-(setq ispell-dictionary "american")
 
 ;; Auto reload files
 (global-auto-revert-mode 1)
@@ -23,26 +20,25 @@
 (show-paren-mode 1)
 
 ;;----------------------------------------------------------------------------
-;; Emacs is Magic
+;; Handy key bindings
 ;;----------------------------------------------------------------------------
 (cua-selection-mode t)
 
 ;; C-h v yas-snippet-dirs
-(require-package 'yasnippet)
-(yas-global-mode 1)
-(setq yas-prompt-functions '(yas-ido-prompt
-                             yas-completing-prompt))
+;(require-package 'yasnippet)
+;(yas-global-mode 1)
+;(setq yas-prompt-functions '(yas-ido-prompt yas-completing-prompt))
 
 (require-package 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
 
+;; p n b f t
 (require-package 'undo-tree)
 (global-undo-tree-mode)
 
 (require-package 'ace-jump-mode)
 (global-set-key (kbd "C-;") 'ace-jump-mode)
 
-;; it's cool
 (require-package 'multiple-cursors)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
@@ -56,7 +52,6 @@
 (put 'narrow-to-region 'disabled nil)
 
 ;; Handy key bindings
-(global-set-key (kbd "C-c j") 'join-line)
 (global-set-key (kbd "C-.") 'set-mark-command)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
